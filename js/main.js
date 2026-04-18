e0f05b217a16bcec70eead0644cb2275acb84efd
seoha376 <seoha376@gmail.com>
2026-04-19 04:35:37 +0900

    Preserve selected theme across pages using localStorage

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 const themeToggleButton = document.getElementById("theme-toggle");
 
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "pink") {
+    document.body.classList.add("pink-theme");
+}
+
 if (themeToggleButton) {
     themeToggleButton.addEventListener("click", function () {
-        document.body.classList.toggle("light-theme");
+        document.body.classList.toggle("pink-theme");
+
+
+        if (document.body.classList.contains("pink-theme")) {
+            localStorage.setItem("theme", "pink");
+        } else {
+            localStorage.setItem("theme", "dark");
+        }
     });
 }
